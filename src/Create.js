402c6ba9913dd9dc2038ad0143db724c5611c4af
Stealthboyy <olajideokepelumi2002@gmail.com>
2023-03-14 00:26:06 +0100

    First commit

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,56 @@
-const Create = () => {
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Create = () => { 
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Dave');
+    const [isPending, setIsPending] = useState(false)
+    const history = useHistory();
+
+    const handleSubmit = (e) =>
+     {
+        e.preventDefault();
+        const blog = {title, body, author};
+        setIsPending(true)
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => 
+        {console.log('new blog added');
+        setIsPending(false)
+        history.push('/')
+    })
+    }
+
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog Title:</label>
+                <input type="text"
+                    required 
+                    value={ title } 
+                    onChange = {(e) => setTitle(e.target.value)}/>
+                <label>Blog Body:</label>
+                <textarea
+                    required 
+                    value={body} 
+                    onChange = {(e) => setBody(e.target.value)}></textarea>    
+                <label>Blog author:</label>
+                <select 
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="Dave">Dave</option>
+                    <option value="Concrete">Concrete</option>
+                </select>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog...</button>}
+            </form>
         </div>
      );
 }
- 
+
 export default Create;
--- src/bloglist.js
+++ src/bloglist.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Bloglist = ({blogs, title}) => {
 
 
@@ -6,8 +8,10 @@ const Bloglist = ({blogs, title}) => {
             <h2>{title}</h2>
             {blogs.map((blog) => (
             <div className="blog-preview" key={blog.id}>
-                <h2>{ blog.title }</h2>
-                <p>Written by {blog.author}</p>
+                <Link to={`/blogs/${blog.id}`}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by {blog.author}</p>
+                </Link>
             </div>          
             ))}
         </div>
